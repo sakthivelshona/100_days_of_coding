b657b4c5fecddcd62b5734faa2eabbdb0771fdeb
Shona S <95616858+sakthivelshona@users.noreply.github.com>
2023-03-16 21:56:59 +0530

    Create day5.java

---
+++ day5.java
@@ -0,0 +1,28 @@
+Java end of file
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        
+        Scanner c=new Scanner(System.in);
+        
+        int i=1;
+        while(c.hasNext()){
+            
+                    String obj=c.nextLine();
+                    System.out.println(i+" "+obj);
+                    ++i;
+
+        }
+        
+        
+    }
+}
