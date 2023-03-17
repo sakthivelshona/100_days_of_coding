d2abc51cca4feacda0d182de213bc5cba1eadb37
Shona S <95616858+sakthivelshona@users.noreply.github.com>
2023-03-17 16:12:04 +0530

    Update day8.java

--- day8.java
+++ day8.java
@@ -1,2 +1,32 @@
 Palindrome in String
 
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        int a = A.length();
+        String reverse="";
+        for(int i=a-1;i>=0;--i){
+            
+            reverse = reverse + A.charAt(i);
+        }        
+        
+        if(A.toLowerCase().equals(reverse.toLowerCase())){
+            
+            System.out.println("Yes");
+        } 
+            else{
+              System.out.println("No");
+
+            
+        }
+    }
+}
+
+
+
