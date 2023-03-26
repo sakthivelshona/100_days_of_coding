b1aaa13264668f8510bec2dc9653d59ef45e695c
Shona S <95616858+sakthivelshona@users.noreply.github.com>
2023-03-26 20:58:28 +0530

    Create day17.java

---
+++ day17.java
@@ -0,0 +1,26 @@
+Java BigInt
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            
+        Scanner p = new Scanner(System.in);
+        BigInteger a = new BigInteger(p.next());
+        BigInteger b = new BigInteger(p.next());
+        
+    
+        BigInteger c = a.add(b);    
+        BigInteger d = a.multiply(b);    
+        
+        System.out.println(c);
+        System.out.println(d);
+
+        
+    }
+}
